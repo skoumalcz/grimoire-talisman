af386c97fa4ffb18b8c344d1f3f7378be7554900
Viktor De Pasquale <info@depasquale.wiki>
2021-01-15 13:19:11 +0100

    Added convenience functions to UseCaseFlow

--- talisman/src/main/java/com/skoumal/grimoire/talisman/UseCaseFlow.kt
+++ talisman/src/main/java/com/skoumal/grimoire/talisman/UseCaseFlow.kt
@@ -4,6 +4,31 @@ import kotlinx.coroutines.flow.Flow
 
 interface UseCaseFlow<In : Any?, Out : Any?> {
 
-    fun observe(input: In): Flow<Out>
+    /**
+     * Allows for a use case to provide flow with values. This is typically for cases where
+     * observing data from database is necessary.
+     *
+     * You should never call this method directly, instead use [observe] for more convenience.
+     * */
+    fun flow(input: In): Flow<Out>
 
 }
+
+/**
+ * Convenience function to rationally call observe on unknown generic flow.
+ * @see UseCaseFlow.flow
+ * */
+fun <In, Out> UseCaseFlow<In, Out>.observe(input: In) = flow(input)
+
+/**
+ * Convenience function to rationally call observe on flow with [Unit] input type.
+ * @see UseCaseFlow.flow
+ * */
+fun <Out> UseCaseFlow<Unit, Out>.observe() = flow
+
+/**
+ * Convenience function to rationally call observe on flow with [Unit] input type.
+ * @see UseCaseFlow.flow
+ * */
+val <Out> UseCaseFlow<Unit, Out>.flow
+    get() = observe(Unit)
